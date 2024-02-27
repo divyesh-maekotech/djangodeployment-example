46c57df2eb336a9d46e82ccbd78e96e8d69320f5
divyesh-maekotech <divyeshgandhi2000@gmail.com>
2024-02-27 20:33:05 +0530

    first commit

---
+++ basic_app/forms.py
@@ -0,0 +1,16 @@
+# learning_users/basic_app/forms.py
+from django import forms
+from basic_app.models import UserProfileInfo
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileInfoForm(forms.ModelForm):
+    class Meta:
+        model = UserProfileInfo
+        fields = ('portfolio_site', 'profile_pic')
